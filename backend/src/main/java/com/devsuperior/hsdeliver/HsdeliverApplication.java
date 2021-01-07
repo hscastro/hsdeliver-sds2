130f8b32484282f8ec149be8fcb9aaec35bbf988
hscastro <halisonsc5@gmail.com>
2021-01-07 10:57:12 -0300

    project created

---
+++ backend/src/main/java/com/devsuperior/hsdeliver/HsdeliverApplication.java
@@ -0,0 +1,13 @@
+package com.devsuperior.hsdeliver;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HsdeliverApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HsdeliverApplication.class, args);
+	}
+
+}
---
+++ backend/src/test/java/com/devsuperior/hsdeliver/HsdeliverApplicationTests.java
@@ -0,0 +1,13 @@
+package com.devsuperior.hsdeliver;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HsdeliverApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
