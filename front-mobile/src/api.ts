9f53f4a8f2caca6fbd910efab93a5435d34cd7f6
hscastro <halisonsc5@gmail.com>
2021-01-15 23:04:36 -0300

    Add OrderDetails

---
+++ front-mobile/src/api.ts
@@ -0,0 +1,13 @@
+import axios from "axios";
+
+const API_URL = 'http://10.0.0.8:8080';
+//const API_URL = 'https://halyson-hsdeliver.herokuapp.com';
+
+export function fetchOrders(){
+    return axios(`${API_URL}/orders`);
+}
+
+
+export function confirmDelivery(orderId: number){
+    return axios.put(`${API_URL}/orders/${orderId}/delivered`);
+}
